f4fb7f9c315dd036530817a13ac1b76f58fae9bf
Daniel Nimmervoll <daniel@nimmervoll.eu>
2017-05-04 15:49:26 +0200

    made a better version
    emre is a noob

--- font_script.sh
+++ font_script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-curl -o ~/Documents/fonts.zip https://transfer.sh/116dnP/fonts.zip
-unzip ~/Documents/fonts.zip
-cp -r ~/Documents/fonts/grotesk2/* /Library/Fonts/
-cp -r ~/Documents/fonts/open_sans/* /Library/Fonts/
-cp -r ~/Documents/fonts/lato/* /Library/Fonts/
+curl -o /tmp/fonts.zip https://transfer.sh/116dnP/fonts.zip
+unzip /tmp/fonts.zip
 
-rm ~/Documents/fonts.zip
-rm -R ~/Documents/fonts/
+mv /tmp/fonts/grotesk2/* /Library/Fonts/
+mv /tmp/fonts/open_sans/* /Library/Fonts/
+mv /tmp/fonts/lato/* /Library/Fonts/
+rm /tmp/fonts.zip
